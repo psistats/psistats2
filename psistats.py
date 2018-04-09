3c790d0be4e07bd9b0ab0c8bc9920d2fc08fd4e8
alex-dow <adow@psikon.com>
2018-04-09 14:24:28 -0400

    HttpOutput plugin

--- psistats.py
+++ psistats.py
@@ -8,6 +8,7 @@ from psistats2.plugins.reporters.network import IPAddrReporter
 from psistats2.plugins.output.stdout import OutputStdout
 from psistats2.plugins.output.logging import OutputLogging
 from psistats2.plugins.output.amqp import AMQPOutput
+from psistats2.plugins.output.http import HttpOutput
 from psistats2 import config
 from psistats2.cli import main
 import time
---
+++ psistats2/plugins/output/http.py
@@ -0,0 +1,28 @@
+from psireporter import OutputPlugin
+import json
+import urllib.request
+import urllib.error
+import logging
+
+class HttpOutput(metaclass=OutputPlugin):
+
+    PLUGIN_ID = 'http'
+
+    def __init__(self):
+        self.logger = logging.getLogger('psistats.http')
+
+    def send(self, report):
+        url = self.config['url']
+        data = json.dumps(dict(report)).encode('utf-8')
+
+        user_agent = 'psistats2'
+
+        headers = {'User-Agent': user_agent,
+                   'Content-type': 'application/json'}
+
+        try:
+            req = urllib.request.Request(url, data=data, headers=headers)
+            res = urllib.request.urlopen(req)
+        except urllib.error.URLError as e:
+            self.logger.error(str(e))
+
