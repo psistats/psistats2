6e8179249d8e4500166cc2cbd517dc6fd23dbc49
Alex Dow <adow@psikon.com>
2018-04-09 19:00:46 -0700

    Some tweaks to the network plugin
    beginnings of windows service

--- psistats2/winservice.py
+++ psistats2/winservice.py
@@ -1,14 +1,21 @@
 import servicemanager
 import socket
-import sys
+import sys, os
 import win32event
 import win32service
 import win32serviceutil
 import time
+import logging
+import io
+import traceback
+from psistats2 import config
+from psistats2.report import PsistatsReport
+from psireporter import Manager
+from logging.config import fileConfig
 
 class PsistatsService(win32serviceutil.ServiceFramework):
-    _svc_name = 'Psistats2'
-    _svc_display_name = 'Psistats2 Monitoring Tool'
+    _svc_name_ = 'Psistats2'
+    _svc_display_name_ = 'Psistats2 Monitoring Tool'
 
     def __init__(self, args):
         win32serviceutil.ServiceFramework.__init__(self, args)
@@ -19,6 +26,7 @@ class PsistatsService(win32serviceutil.ServiceFramework):
         socket.setdefaulttimeout(60)
 
     def SvcStop(self):
+        servicemanager.LogInfoMsg('Stopping service')
         self.ReportServiceStatus(win32service.SERVICE_STOP_PENDING)
         win32event.SetEvent(self.evt_stop)
         self.need_to_stop = True
@@ -28,15 +36,46 @@ class PsistatsService(win32serviceutil.ServiceFramework):
         servicemanager.LogMsg(
             servicemanager.EVENTLOG_INFORMATION_TYPE,
             servicemanager.PYS_SERVICE_STARTED,
-            (self._svc_name, '')
+            (self._svc_name_, '')
         )
 
-        self.main()
+        try:
+            self.main()
+        except Exception as e:
+            output = io.StringIO()
+            
+            traceback.print_tb(e.__traceback__, file=output)
+
+            servicemanager.LogInfoMsg('ERROR: %s' % e)
+            servicemanager.LogInfoMsg('ERROR: %s' % output.getvalue())
+            
+            output.close()
 
     def main(self):
 
+        home_dir = 'c:\\Users\\IEUser'
+    
+        servicemanager.LogInfoMsg('HOME DIR: %s' % home_dir)
+    
+        conffile = os.path.join(home_dir, 'psistats.conf')
+        
+        conf = config.load(conffile)
+        fileConfig(conffile)
+        
+        logger = logging.getLogger('psistats2')
+        logger.info('Starting psistats2')
+    
+        servicemanager.LogInfoMsg('Starting Manager thread')
+        
+        manager = Manager(conf, reportClass=PsistatsReport)
+        manager.start()
+        
         while True:
             if self.need_to_stop is True:
                 break
             time.sleep(5)
-
+        
+        servicemanager.LogInfoMsg('Stopping Manager thread')
+        manager.stop()
+    
+        servicemanager.LogInfoMsg('Done')
