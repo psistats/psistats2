b30aaf1808114b322fc68418dfa2fb7b63b6f96f
Alex Dow <adow@psikon.com>
2019-05-06 23:34:17 -0400

    Building tweaks

--- building/debian/dependencies.sh
+++ building/debian/dependencies.sh
@@ -14,10 +14,22 @@ FPM_OPTS="$FPM_OPTS --python-package-name-prefix=python3"
 FPM_OPTS="$FPM_OPTS --python-install-lib=/usr/lib/python3/dist-packages"
 
 cd $PROJECT_DIR
+rm -rf $PROJECT_DIR/dist
+
+mkdir $PROJECT_DIR/dist
+mkdir $PROJECT_DIR/dist/debian_dependencies
+
+cd $PROJECT_DIR/dist/debian_dependencies
 
 while IFS= read -r var
 do
+    PKG_VERSION="$(echo $var | awk -F'>=' '{print $2}')"
+
+    PKG_VERSION="$PKG_VERSION~psikon"
+    PKG_VERSION="$PKG_VERSION$BUILD_NUMBER"
+
+    echo "Version: $PKG_VERSION"
     echo "$var"
-    fpm $FPM_OPTS $var
-done < "requirements.txt"
+    fpm $FPM_OPTS --version $PKG_VERSION $var
+done < "$PROJECT_DIR/requirements.txt"
 
