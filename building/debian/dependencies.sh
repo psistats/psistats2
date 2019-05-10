61fce015f2dccbf9dd5b0a6cc3c5cd83d335de29
Alex Dow <adow@psikon.com>
2019-05-09 22:42:41 -0400

    Changed to dependencies builder

--- building/debian/dependencies.sh
+++ building/debian/dependencies.sh
@@ -11,7 +11,7 @@ echo "Project: $PROJECT_DIR"
 
 FPM_OPTS="--python-bin python3 -s python -t deb"
 FPM_OPTS="$FPM_OPTS --python-package-name-prefix=python3"
-FPM_OPTS="$FPM_OPTS --python-install-lib=/usr/local/lib/python3/dist-packages"
+FPM_OPTS="$FPM_OPTS --python-install-lib=/usr/lib/python3/dist-packages"
 
 cd $PROJECT_DIR
 rm -rf $PROJECT_DIR/dist
@@ -25,11 +25,11 @@ while IFS= read -r var
 do
     PKG_VERSION="$(echo $var | awk -F'>=' '{print $2}')"
 
-    PKG_VERSION="$PKG_VERSION~psikon"
+    PKG_VERSION="$PKG_VERSION$PREFIX"
     PKG_VERSION="$PKG_VERSION$BUILD_NUMBER"
 
     echo "Version: $PKG_VERSION"
     echo "$var"
-    fpm $FPM_OPTS --version $PKG_VERSION $var
+    fpm $FPM_OPTS -v $PKG_VERSION $var
 done < "$PROJECT_DIR/requirements.txt"
 
